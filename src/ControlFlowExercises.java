aff7146181dd0b9581014bbcb60884090f32d93b
William Mejia <willx1995@gmail.com>
2022-03-28 16:02:29 -0500

    adding ControlFlowExercises.java

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,31 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        //1. basic loops
+
+        //a.
+
+//        int i = 5;
+//
+//        while(i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        //For Loop
+//        for(int i = 5; i <= 15; i++){
+//            System.out.println(i);
+//        }
+
+        //b.
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i <= 100);
+
+        //For Loop
+//        for (int i = 0; i <= 100; i+= 2){
+//            System.out.println(i);
+//        }
+    }
+}
