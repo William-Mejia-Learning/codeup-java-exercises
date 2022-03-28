0fae496522b0b32df2482b2c0a2397adfa7d410c
William Mejia <willx1995@gmail.com>
2022-03-28 16:54:50 -0500

    FizzBuzz solution for ControlFlowExercises.java

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -44,6 +44,22 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        };
 
+        //2. FizzBuzz
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 != 0 && i % 5 != 0) {
+                System.out.println(i);
+            }
+            else if (i % 5 == 0 & i % 3 == 0) {
+                System.out.println(i + " FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                System.out.println(i + " Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println(i + " Buzz");
+            }
+        }
+
 
     }
 }
