8dd23c2f611160baf4ad7818d2d9ca942de2fb1a
William Mejia <willx1995@gmail.com>
2022-03-28 19:00:06 -0500

    commitng controlStatementsAndLoopsLecture.java and ipdating ControlFlowExercises.java to pull on my linux machine

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -45,20 +45,20 @@ public class ControlFlowExercises {
 //        };
 
         //2. FizzBuzz
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 != 0 && i % 5 != 0) {
-                System.out.println(i);
-            }
-            else if (i % 5 == 0 & i % 3 == 0) {
-                System.out.println(i + " FizzBuzz");
-            }
-            else if (i % 3 == 0) {
-                System.out.println(i + " Fizz");
-            }
-            else if (i % 5 == 0) {
-                System.out.println(i + " Buzz");
-            }
-        }
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 != 0 && i % 5 != 0) {
+//                System.out.println(i);
+//            }
+//            else if (i % 5 == 0 & i % 3 == 0) {
+//                System.out.println(i + " FizzBuzz");
+//            }
+//            else if (i % 3 == 0) {
+//                System.out.println(i + " Fizz");
+//            }
+//            else if (i % 5 == 0) {
+//                System.out.println(i + " Buzz");
+//            }
+//        }
 
 
     }
