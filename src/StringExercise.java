79517abf3a39c8b88792ab56fe483ac730143280
William Mejia <willx1995@gmail.com>
2022-03-29 14:43:16 -0500

    commiting StringExercise.java to repository

---
+++ src/StringExercise.java
@@ -0,0 +1,18 @@
+public class StringExercise {
+    public static void main(String[] args){
+        //1. String basics
+        String str = "We don't need no ";
+
+        System.out.println(str + "education");
+        System.out.println(str + "thought control");
+
+        String check = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(check);
+
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
+        String backLash = "I can do backslashes \\, double backlashes \\\\ and the amazing triple backlash \\\\\\";
+        System.out.println(backLash);
+    }
+}
