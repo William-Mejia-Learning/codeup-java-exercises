955eadcd6b49dce9dbd98db0e52247cd48552b97
William Mejia <willx1995@gmail.com>
2022-03-29 15:21:33 -0500

    adding Bob.java to repository

---
+++ src/Bob.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args){
+        String question = "Sure";
+        String yell = "Whoa, chill out!";
+        String address = "Fine. Be that way!";
+
+        String talk;
+        do{
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Talk to Bob");
+            talk = sc.nextLine();
+            if(talk.endsWith("?")){
+                System.out.println(question);
+            } else if(talk.endsWith("!")){
+                System.out.println(yell);
+            } else if(talk.endsWith(" ")){
+                System.out.println(address);
+            } else{
+                System.out.println("Whatever");
+            }
+        }while(talk.endsWith("?") || talk.endsWith("!") ||talk.endsWith(""));
+
+    }
+}
