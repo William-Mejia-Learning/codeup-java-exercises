f4a6b7a3ebfa5a6a61a2dc71bcc707af51e9f105
William Mejia <willx1995@gmail.com>
2022-03-30 16:38:56 -0500

    adding solutions for exercise 3 in the MethodsExercises.java class

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -15,60 +15,78 @@ public class MethodsExercises {
         //d.
 //        modulus(4, 2);
 //        modulus(3, 2);
-        System.out.print("Enter a number between 1 and 10: ");
-        System.out.println(getInteger(1, 10));
+//        System.out.print("Enter a number between 1 and 10: ");
+//        System.out.println(getInteger(1, 10));
+
+        System.out.println("Enter number between 1 and 10:");
+        getFactorials();
 
     }
 
     //1. Basic Arithmetic
     //a. Create four separate methods. Each will perform an arithmetic operation:
     //b. Each function needs to take two parameters/arguments so that the operation can be performed.
-    public static int addition(int a, int b){
-//        System.out.println(a + b);
-        return a + b;
-    }
-
-    public static void subtraction(int a, int b){
-        System.out.println(a - b);
-    }
-
-    public static void multiplication(int a, int b){
-//        System.out.println(a * b);
-        //bonus
-//        System.out.print("This is the recursion one: ");
-        int mult = 0;
-        int i;
-        for(i = 1; i <= a;i++)
-        {
-            mult=mult+b;
-        }
-        System.out.println(mult);
-    }
-
-    public static void division(double a, double b){
-        System.out.println(a / b);
-    }
-
-    //d. Add a modulus method that finds the modulus of two numbers.
-    public static void modulus(double a, double b){
-        System.out.println(a % b);
-    }
-
-    //2
-    public static int getInteger(int min, int max){
-        Scanner sc = new Scanner(System.in);
-        int userInput = sc.nextInt();
-        if(userInput >= min && userInput <= max){
-            return userInput;
-        }
-        System.out.print("Number not valid ");
-
-        return getInteger(min, max);
-    }
+//    public static int addition(int a, int b){
+////        System.out.println(a + b);
+//        return a + b;
+//    }
+//
+//    public static void subtraction(int a, int b){
+//        System.out.println(a - b);
+//    }
+//
+//    public static void multiplication(int a, int b){
+////        System.out.println(a * b);
+//        //bonus
+////        System.out.print("This is the recursion one: ");
+//        int mult = 0;
+//        int i;
+//        for(i = 1; i <= a;i++)
+//        {
+//            mult=mult+b;
+//        }
+//        System.out.println(mult);
+//    }
+//
+//    public static void division(double a, double b){
+//        System.out.println(a / b);
+//    }
+//
+//    //d. Add a modulus method that finds the modulus of two numbers.
+//    public static void modulus(double a, double b){
+//        System.out.println(a % b);
+//    }
+//
+//    //2
+//    public static int getInteger(int min, int max){
+//        Scanner sc = new Scanner(System.in);
+//        int userInput = sc.nextInt();
+//        if(userInput >= min && userInput <= max){
+//            return userInput;
+//        }
+//        System.out.print("Number not valid ");
+//
+//        return getInteger(min, max);
+//    }
 
 
     //3
 
-    
+        public static int getFactorials() {
+            Scanner sc =new Scanner(System.in);
+            int i;
+            int factorial = 1;
+            int userInput = sc.nextInt();
+            if(userInput >= 1 && userInput <=10){
+                for(i =1; i <= userInput; i++){
+                    factorial = factorial * i;
+                }
+                System.out.println(factorial);
+                return factorial;
+            }
+            System.out.println("[Invalid Number]");
+            System.out.println("Enter number between 1 and 10:");
+            return getFactorials();
+        }
 
 }
