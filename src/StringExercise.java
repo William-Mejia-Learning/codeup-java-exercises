7e621b7d50eedd87c02633765fad1bc719018610
William Mejia <willx1995@gmail.com>
2022-03-30 10:22:44 -0500

    making some changes to the StringExercise.java file

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,10 +1,9 @@
 public class StringExercise {
     public static void main(String[] args){
         //1. String basics
-        String str = "We don't need no ";
-
-        System.out.println(str + "education");
-        System.out.println(str + "thought control");
+        String str = "We don't need no education \n";
+        str += "We don't need not thought control";
+        System.out.println(str);
 
         String check = "Check \"this\" out!, \"s inside of \"s!";
         System.out.println(check);
@@ -12,7 +11,7 @@ public class StringExercise {
         String windows = "In windows, the main drive is usually C:\\";
         System.out.println(windows);
 
-        String backLash = "I can do backslashes \\, double backlashes \\\\ and the amazing triple backlash \\\\\\";
+        String backLash = "I can do backslashes \\, double backlashes \\\\, \n and the amazing triple backlash \\\\\\";
         System.out.println(backLash);
     }
 }
