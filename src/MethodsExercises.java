ad272c5625cdb817de21faf9c945f35e76909d58
William Mejia <willx1995@gmail.com>
2022-03-30 15:55:18 -0500

    adding solutions for exercise 2 in MethodsExercises.java

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,20 +1,23 @@
+import javax.print.MultiDocPrintService;
 import java.util.Scanner;
 
 public class MethodsExercises {
     public static void main(String[] args){
-        Scanner sc =new Scanner(System.in);
+//        Scanner sc =new Scanner(System.in);
         //1. Basic Arithmetic
         //c. Test your methods and verify the output.
-        System.out.println(addition(3,2));
-        subtraction(3,2);
-        multiplication(3,2);
-        division(3,2);
-        division(3,0);// Dividing by zero gives infinity
+//        System.out.println(addition(3,2));
+//        subtraction(3,2);
+//        multiplication(3,2);
+//        division(3,2);
+//        division(3,0);// Dividing by zero gives infinity
 
         //d.
-        modulus(4, 2);
-        modulus(3, 2);
-        modulus(1, 2);
+//        modulus(4, 2);
+//        modulus(3, 2);
+        System.out.print("Enter a number between 1 and 10: ");
+        System.out.println(getInteger(1, 10));
+
     }
 
     //1. Basic Arithmetic
@@ -29,8 +32,10 @@ public class MethodsExercises {
         System.out.println(a - b);
     }
 
-    public static void multiplyLoop(int a, int b){
-        //Bonus
+    public static void multiplication(int a, int b){
+//        System.out.println(a * b);
+        //bonus
+//        System.out.print("This is the recursion one: ");
         int mult = 0;
         int i;
         for(i = 1; i <= a;i++)
@@ -38,13 +43,6 @@ public class MethodsExercises {
             mult=mult+b;
         }
         System.out.println(mult);
-    }
-
-    public static void multiplication(int a, int b){
-//        System.out.println(a * b);
-        //bonus
-        System.out.print("This is the recursion one: ");
-        multiplyLoop(a, b);
     }
 
     public static void division(double a, double b){
@@ -55,4 +53,22 @@ public class MethodsExercises {
     public static void modulus(double a, double b){
         System.out.println(a % b);
     }
+
+    //2
+    public static int getInteger(int min, int max){
+        Scanner sc = new Scanner(System.in);
+        int userInput = sc.nextInt();
+        if(userInput >= min && userInput <= max){
+            return userInput;
+        }
+        System.out.print("Number not valid ");
+
+        return getInteger(min, max);
+    }
+
+
+    //3
+
+    
+
 }
