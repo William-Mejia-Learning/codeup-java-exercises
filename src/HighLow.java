12c3085941388a86eb76856c96bcac6cda70d906
William Mejia <willx1995@gmail.com>
2022-03-31 09:31:57 -0500

    refactoring, adding a you lost message, changing the number of tries to 20 in HighLow.java class

--- src/HighLow.java
+++ src/HighLow.java
@@ -7,26 +7,15 @@ public class HighLow {
         Scanner sc = new Scanner(System.in);
         System.out.println("Guess a number from 1 - 100:");
         guessNumber();
-//        int rand = (int) (Math.random() * 6) + 1;
-//        System.out.println(rand);
     }
-
-//    public static int generateRandomNumber() {
-//        Random rand = new Random();
-//        int upperbound = 5;
-//        int lowerBound = 1;
-//        int int_random = rand.nextInt(upperbound + 1 - lowerBound) + lowerBound;
-//        System.out.println(int_random);
-//        return int_random;
-//    }
 
     public static void guessNumber() {
         Scanner sc = new Scanner(System.in);
         int rand = (int) (Math.random() * 100) + 1;
-        int count = 100;
+        int count = 20;
         int num;
         int i;
-        for (i = 1; i <= 100; i++) {
+        for (i = 1; i <= 20; i++) {
              num = sc.nextInt();
 //            System.out.println(rand);
             if (num == rand) {
@@ -38,6 +27,10 @@ public class HighLow {
             } else if (num > rand) {
                 count--;
                 System.out.println("TOO HIGH");
+            }
+            if(count == 0){
+                System.out.println("YOU LOST");
+                break;
             }
             System.out.println("Tries left: " + count);
         }
